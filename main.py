692c770c3189a403f3d2bd11282821faf76fe86b
lane-fuerstenberg <lane.fuerstenberg@protonmail.com>
2020-05-21 11:29:16 -0500

    Fixing discord message to not output as list and instead output as string

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ async def on_ready():
 
 @bot.command()
 async def combo(ctx, *args):
-    converted_combo = combo_generator.convert(args)
+    combo_list = combo_generator.convert(args)
+    converted_combo = " ".join(str(x) for x in combo_list
     await ctx.send(converted_combo)
 
 
