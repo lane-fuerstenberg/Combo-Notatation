04456037ed6942984e29c66707d4f568f8e5c256
lane-fuerstenberg <lane.fuerstenberg@protonmail.com>
2020-05-21 10:34:00 -0500

    Updating replace_all_inputs_in() method and adding emotes to Input class

--- combo_generator.py
+++ combo_generator.py
@@ -1,4 +1,6 @@
 from input import Input
+import re
+
 
 def convert(args):
     parsed_combo = []
@@ -9,6 +11,28 @@ def convert(args):
     return parsed_combo
 
 
-def replace_all_inputs_in(string):
-    updated_string = string.replace('uf', Input.WHITE_UF.value)
-    return updated_string
+def replace_all_inputs_in(word):
+    for keys in Input.keys():
+
+        # looks pretty bad but python automatically converts 1 length tuples into strings
+        # this messes up the code if it does not validate that it has a tuple first (might be a better solution)
+        if type(keys) is str:
+            word = replace_with_string(keys, word)
+        else:
+            word = replace_with_tuple(keys, word)
+
+    return word
+
+
+def replace_with_string(keys, word):
+    if re.search(r'\b{0}'.format(keys), word):
+        word = word.replace(keys, Input.get(keys))
+    return word
+
+
+def replace_with_tuple(keys, word):
+    for key in keys:
+        if re.search(r'\b{0}'.format(key), word):
+            word = word.replace(key, Input.get(keys))
+
+    return word
