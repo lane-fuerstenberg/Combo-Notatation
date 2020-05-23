d543185a67f022fc5b62f9af128fac81551c2c78
lane-fuerstenberg <lane.fuerstenberg@protonmail.com>
2020-05-23 06:23:39 -0500

    Adding comments for readability

--- combo_generator.py
+++ combo_generator.py
@@ -5,16 +5,18 @@ import re
 def convert(args):
     combo_list = []
     for arg in args:
-        combo_list.append(replace_matching_key(arg))
+        combo_list.append(replace_matching_keys_in_word(arg))
 
     return combo_list
 
 
-def replace_matching_key(word):
+def replace_matching_keys_in_word(word):
+    # regex for avoiding reading emote data as key
     regex_string = generate_regex_string()
 
     for keys, value in Input.items():
         for key in keys:
+            # replaces + for \+ because it needs + to be read as literal
             regex = re.compile(regex_string[0] + key.replace("+", "\+") + regex_string[1])
             search = regex.search(word)
 
@@ -39,6 +41,7 @@ def get_matching_key(keys, word):
 
 
 def generate_regex_string():
+    # purpose of this method is to generate regex that will not read discord emote data as a key
     any_char_regex = r"[A-Z,a-z,0-9,+]*"
     starting_regex_string = r"(\A|<^|>)" + any_char_regex
     ending_regex_string = any_char_regex + r"(\Z|>^|<)"
